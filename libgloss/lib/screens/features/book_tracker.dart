0913ef03851abc2814043a5df25d34ba6385a1b3
MiliGuagu <is728351@iteso.mx>
2022-10-09 00:32:06 -0500

    Mocked lists for books and new purple color

--- libgloss/lib/screens/features/book_tracker.dart
+++ libgloss/lib/screens/features/book_tracker.dart
@@ -7,14 +7,57 @@ import '../../widgets/side_menu.dart';
 
 class BookTracker extends StatelessWidget {
   final Color _primaryColor = Color.fromRGBO(244, 210, 255, 1);
-  final Color _secondaryColor = Color.fromRGBO(192, 85, 229, 1);
+  final Color _secondaryColor = Color.fromRGBO(215, 132, 243, 1);
   final Color _blueColor = Color.fromRGBO(16, 112, 130, 1);
   
   final TextEditingController _textFieldController = TextEditingController();
 
   BookTracker({super.key});
 
-  var mockedData = {};
+  final List<Map<String, String>> _listSeguimientos = [
+    {
+      "title": "Pet Sematary",
+      "author": "Stephen King",
+      "image": "https://m.media-amazon.com/images/I/713xSwL4TyL.jpg",
+      "precio": "150.00",
+      "plataforma": "cualquier plataforma",
+      "tiempo": "3 meses",
+    },
+    {
+      "title": "Loveless",
+      "author": "Alice Oseman",
+      "image": "https://m.media-amazon.com/images/I/61PpXmjK2KL.jpg",
+      "precio": "250.00",
+      "plataforma": "cualquier plataforma",
+      "tiempo": "6 meses",
+    },
+    {
+      "title": "Bloom",
+      "author": "Kevin Panetta",
+      "image": "https://m.media-amazon.com/images/I/91Sia1ZLldL.jpg",
+      "precio": "190.00",
+      "plataforma": "cualquier plataforma",
+      "tiempo": "4 meses",
+    },
+  ];
+
+  final List<Map<String, String>> _wishList = [
+    {
+      "title": "Gone Girl",
+      "author": "Gillian Flynn",
+      "image": "https://m.media-amazon.com/images/I/81g5ooiHAXL.jpg",
+    },
+    {
+      "title": "The Girl from the Sea",
+      "author": "Molly Knox Ostertag",
+      "image": "https://m.media-amazon.com/images/I/71Dnpq3Tt-L.jpg",
+    },
+    {
+      "title": "The Lord of the Rings",
+      "author": "J.R.R. Tolkien",
+      "image": "https://m.media-amazon.com/images/I/51kfFS5-fnL._SX332_BO1,204,203,200_.jpg",
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
