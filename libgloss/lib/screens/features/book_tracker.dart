932a13b4ed1ff2a24de1e15529742ffbd8e30ab6
Rodrigo Zamora <71241681+rodrigo-zamora@users.noreply.github.com>
2022-10-09 00:19:02 -0500

    Update book_tracker.dart

--- libgloss/lib/screens/features/book_tracker.dart
+++ libgloss/lib/screens/features/book_tracker.dart
@@ -3,6 +3,7 @@ import 'package:libgloss/config/routes.dart';
 
 import '../../widgets/bottom_navigation.dart';
 import '../../widgets/search_appbar.dart';
+import '../../widgets/side_menu.dart';
 
 class BookTracker extends StatelessWidget {
   final Color _primaryColor = Color.fromRGBO(244, 210, 255, 1);
@@ -10,6 +11,8 @@ class BookTracker extends StatelessWidget {
   final TextEditingController _textFieldController = TextEditingController();
 
   BookTracker({super.key});
+
+  var mockedData = {};
 
   @override
   Widget build(BuildContext context) {
@@ -24,10 +27,72 @@ class BookTracker extends StatelessWidget {
           showCameraButton: false,
         ),
       ),
-      body: Container(),
+      drawer: SideMenu(
+        sideMenuColor: _primaryColor,
+      ),
+      body: _buildTrackerBody(),
       bottomNavigationBar: BottomNavigation(
           selectedItem: LibglossRoutes.BOOK_TRACKER,
           iconColor: _secondaryColor),
     );
+  }
+
+  Widget _trackingWidget() {
+    return Column(
+      children: [
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          'Seguimientos',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Container(
+                  color: _primaryColor,
+                  width: 150,
+                  child: Text('Seguimiento 1'),
+                ),
+                Container(
+                  color: _secondaryColor,
+                  width: 150,
+                  child: Text(
+                    'Seguimiento 2',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  color: _primaryColor,
+                  width: 150,
+                  child: Text('Seguimiento 3'),
+                ),
+              ],
+            )),
+      ],
+    );
+  }
+
+  Widget _wishListWidget() {
+    return Container(
+      color: _secondaryColor,
+    );
+  }
+
+  Widget _buildTrackerBody() {
+    return Column(children: <Widget>[
+      Expanded(child: _trackingWidget()),
+      Expanded(child: _wishListWidget()),
+    ]);
   }
 }
