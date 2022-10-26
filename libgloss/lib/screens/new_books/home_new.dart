114bde739632421e3a7d02209ddd32cbb87c29c3
Rodrigo Zamora <71241681+rodrigo-zamora@users.noreply.github.com>
2022-10-26 13:01:51 -0500

    Added pull to refresh in home new

--- libgloss/lib/screens/new_books/home_new.dart
+++ libgloss/lib/screens/new_books/home_new.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:libgloss/blocs/books/bloc/books_bloc.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
+import 'package:libgloss/blocs/books/bloc/books_bloc.dart';
 import 'package:libgloss/config/routes.dart';
 import 'package:libgloss/widgets/shared/online_image.dart';
 import 'package:libgloss/widgets/shared/side_menu.dart';
@@ -44,79 +45,131 @@ class _HomeNewState extends State<HomeNew> {
     );
   }
 
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
+  void _onRefresh() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    BlocProvider.of<BooksBloc>(context).add(GetTopBooksEvent());
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
+  }
+
+  void _onLoading() async {
+    // monitor network fetch
+    BlocProvider.of<BooksBloc>(context).add(GetTopBooksEvent());
+    // if failed,use loadFailed(),if no data return,use LoadNodata()
+    if (mounted) setState(() {});
+    _refreshController.loadComplete();
+  }
+
+  // TODO: Update books list on scroll up
   Column _found(BuildContext context, List<dynamic> books) {
     return Column(
       children: [
         Expanded(
           child: SizedBox(
-            child: GridView.builder(
-              padding: EdgeInsets.all(20),
-              scrollDirection: Axis.vertical,
-              physics: ScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 18,
-                childAspectRatio: MediaQuery.of(context).size.width /
-                    (MediaQuery.of(context).size.height / 1.5),
+            child: SmartRefresher(
+              enablePullUp: false,
+              enablePullDown: true,
+              header: WaterDropMaterialHeader(
+                backgroundColor: _secondaryColor,
+                color: Colors.white,
               ),
-              itemCount: books.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  color: Colors.teal[100],
-                  child: Column(
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          BlocProvider.of<BookPriceBloc>(context).add(
-                            GetBookPriceEvent(
-                              bookId: books[index]["isbn"],
+              footer: CustomFooter(
+                builder: (BuildContext context, LoadStatus? mode) {
+                  Widget body;
+                  if (mode == LoadStatus.idle) {
+                    body = Text("pull up load");
+                  } else if (mode == LoadStatus.loading) {
+                    body = CircularProgressIndicator();
+                  } else if (mode == LoadStatus.failed) {
+                    body = Text("Load Failed!Click retry!");
+                  } else if (mode == LoadStatus.canLoading) {
+                    body = Text("release to load more");
+                  } else {
+                    body = Text("No more Data");
+                  }
+                  return Container(
+                    height: 55.0,
+                    child: Center(child: body),
+                  );
+                },
+              ),
+              controller: _refreshController,
+              onRefresh: _onRefresh,
+              onLoading: _onLoading,
+              child: GridView.builder(
+                padding: EdgeInsets.all(20),
+                scrollDirection: Axis.vertical,
+                physics: ScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 18,
+                  childAspectRatio: MediaQuery.of(context).size.width /
+                      (MediaQuery.of(context).size.height / 1.5),
+                ),
+                itemCount: books.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    color: Colors.teal[100],
+                    child: Column(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            BlocProvider.of<BookPriceBloc>(context).add(
+                              GetBookPriceEvent(
+                                bookId: books[index]["isbn"],
+                              ),
+                            );
+                            Navigator.pushNamed(
+                              context,
+                              LibglossRoutes.NEW_BOOK_DETAILS,
+                              arguments: books[index],
+                            );
+                          },
+                          child: Container(
+                            color: Colors.pink[100],
+                            height: (MediaQuery.of(context).size.height / 4.7),
+                            child: OnlineImage(
+                              imageUrl: books[index]["thumbnail"] ??
+                                  "https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/A_dictionary_of_the_Book_of_Mormon.pdf/page170-739px-A_dictionary_of_the_Book_of_Mormon.pdf.jpg",
+                              width:
+                                  MediaQuery.of(context).size.width / 2.5, //100
                             ),
-                          );
-                          Navigator.pushNamed(
-                            context,
-                            LibglossRoutes.NEW_BOOK_DETAILS,
-                            arguments: books[index],
-                          );
-                        },
-                        child: Container(
-                          color: Colors.pink[100],
-                          height: (MediaQuery.of(context).size.height / 4.7),
-                          child: OnlineImage(
-                            imageUrl: books[index]["thumbnail"]!,
-                            width:
-                                MediaQuery.of(context).size.width / 2.5, //100
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Text(
-                        "${books[index]["title"]}",
-                        overflow: TextOverflow.ellipsis,
-                        textAlign: TextAlign.center,
-                        maxLines: 2,
-                        style: TextStyle(
-                          fontSize: 14,
+                        SizedBox(
+                          height: 8,
                         ),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        "${books[index]["authors"]}",
-                        overflow: TextOverflow.ellipsis,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: _blueColor,
-                          fontSize: 12,
+                        Text(
+                          "${books[index]["title"]}",
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                          maxLines: 2,
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                );
-              },
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "${books[index]["authors"]}",
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: _blueColor,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
             ),
           ),
         ),
@@ -137,6 +190,7 @@ class _HomeNewState extends State<HomeNew> {
       },
       builder: (context, state) {
         if (state is BooksLoading) {
+          // TODO: Add shimmer effect
           return Center(
             child: CircularProgressIndicator(),
           );
