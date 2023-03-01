0351c0696cfeb37a47e7e4b86f2d9f9ef2df25f9
Rodrigo Zamora <71241681+rodrigo-zamora@users.noreply.github.com>
2023-02-28 21:23:05 -0600

    env

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -37,7 +37,19 @@ process.on('SIGINT', function () {
 
 // Firebase connection
 const admin = require('firebase-admin');
-const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
+const serviceAccount = {
+    'type': 'service_account',
+    'project_id': 'your-project-id',
+    'private_key_id': process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+    // See: https://stackoverflow.com/a/50376092/3403247.
+    'private_key': (process.env.FIREBASE_ADMIN_PRIVATE_KEY).replace(/\\n/g, '\n'),
+    'client_email': process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+    'client_id': process.env.FIREBASE_ADMIN_CLIENT_ID,
+    'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
+    'token_uri': 'https://oauth2.googleapis.com/token',
+    'auth_provider_x509_cert_url': process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+    'client_x509_cert_url': process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
+  };
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
