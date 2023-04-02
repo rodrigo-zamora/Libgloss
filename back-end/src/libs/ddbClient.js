5a02df72446fff9ddccb99e4ff5f91be2a5f2453
Rodrigo Zamora <71241681+rodrigo-zamora@users.noreply.github.com>
2023-04-02 12:32:24 -0600

    Backend integration with DynamoDB

--- back-end/src/libs/ddbClient.js
+++
@@ -1,7 +0,0 @@
-const DynamoDBClient = require('aws-sdk/clients/dynamodb');
-
-const REGION = "us-east-1";
-
-const ddbClient = new DynamoDBClient({ region: REGION });
-
-module.exports = ddbClient;
--- back-end/src/libs/dynogels.js
+++
@@ -1,7 +0,0 @@
-const dynogels = require('dynogels');
-
-const path = require('path');
-
-dynogels.AWS.config.loadFromPath(path.resolve('credentials.json'));
-
-module.exports = dynogels;
